b686ba74bf9ee683d26384893bea3be9eef73456
diego.bustamante@utec.edu.pe <91268999+NewRenTL@users.noreply.github.com>
2023-12-25 01:11:28 -0500

    This is my fifth where I push to my remote repository my last file.java

--- src/TaskCodiGoDiego_1/TaskExercise_5.java
+++ src/TaskCodiGoDiego_1/TaskExercise_5.java
@@ -3,7 +3,7 @@ package TaskCodiGoDiego_1;
 import java.util.Scanner;
 
 public class TaskExercise_5 {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         long m;
